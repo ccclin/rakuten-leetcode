14d00f89ba2b9520e4378e81b39cfa56bd7ae3e0
ccclin <demeter.yeh@gmail.com>
2021-07-13 12:01:25 +0800

    use bit operator

--- rakuten_b_q5/main.go
+++ rakuten_b_q5/main.go
@@ -3,17 +3,18 @@ package main
 import "fmt"
 
 func Add(a, b int) (c int) {
-	sliceA := make([]int, a)
-	sliceB := make([]int, b)
-
-	sliceC := append(sliceA, sliceB...)
-	c = len(sliceC)
+	for b != 0 {
+		c = a ^ b
+		b = (a & b) << 1
+		a = c
+	}
+	c = a
 	return
 }
 
 func main() {
-	a := 1
-	b := 2
+	a := 11
+	b := 1
 	ans := Add(a, b)
 	fmt.Printf("%d + %d = %d", a, b, ans)
 }
